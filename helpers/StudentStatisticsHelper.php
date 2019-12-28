7075b02ac578e2a6e7f339c86f517b4bf9064a3f
Milos Jerkovic <milos.jerkovic@limundo.com>
2019-12-28 23:03:48 +0100

    calculate result for other schooll board

--- helpers/StudentStatisticsHelper.php
+++ helpers/StudentStatisticsHelper.php
@@ -15,13 +15,13 @@ class StudentStatisticsHelper {
     public function calculateStatistics()
     {
         
-        $average_grade = $this->calculateGradesaverage($this->student->grades);
+        $average_grade = $this->calculateGradesAverage($this->student->grades);
         $final_result = $this->claculateFinalResult();
         $this->response_data = ['student' => $this->student, 'average_grade' => $average_grade, 'final_result' => $final_result];
         return $this->response_data;
     }
     
-    public function calculateGradesaverage($grades)
+    public function calculateGradesAverage($grades)
     {
         
         $count = count($grades);
@@ -45,7 +45,51 @@ class StudentStatisticsHelper {
             }
             return 'Passed';
         }
+        else{
+            $count = count($this->student->grades);
+            $grade_objects_array = $this->student->grades;
+            $max_grade = $this->getMaxGrade();
+            $min_grade = $this->getMinGrade();
+
+            if($count > 2){
+                foreach($grade_objects_array as $key=>$grade){
+                    if($min_grade == $grade->value){
+                        unset($grade_objects_array[$key]);
+                    }
+                }
+                if($max_grade < 8){
+                return 'Failed';
+                }
+                return 'Passed';
+            }
+            else{
+                if($max_grade < 8){
+                return 'Failed';
+                }
+                return 'Passed';
+            }
+        }
+    }
+    
+    
+    public function getMaxGrade()
+    {
+        $grade_values_array = [];
+        foreach($this->student->grades as $grade){
+            $grade_values_array[] = $grade->value;
+        }
         
+        return max($grade_values_array);
+    }
+    
+    public function getMinGrade()
+    {
+        $grade_values_array = [];
+        foreach($this->student->grades as $grade){
+            $grade_values_array[] = $grade->value;
+        }
+        
+        return min($grade_values_array);
     }
     
 }
